db9fb360ef042975096d984fba01c830646658b8
lgs777 <jeongc3@naver.com>
2018-03-03 19:59:28 +0900

    0303

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public ItemBox[] itemBoxs;
+	public bool IsGameOver;
+		
+	// Use this for initialization
+	void Start () {
+		IsGameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//리턴은 함수가 리턴값을 돌려준다는 의미도 있지만 동시에 리턴이 나왔기 때문에 함수가 종료가 됨.
+		if (IsGameOver == true)
+		{
+			return;
+		}
+
+
+		int count = 0;
+		for (int i = 0; i < 3; i++) 
+		{
+			
+			if (itemBoxs[i].isOverlaped == true) 
+			{
+				count++;
+			}
+		}
+
+		if (count >= 3) 
+		{
+			Debug.Log ("게임승리");
+			IsGameOver = true;
+		}
+	}
+}
---
+++ Assets/ItemBox.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour {
+
+	public bool isOverlaped = false;
+
+	private Renderer myRenderer;
+
+	public Color touchColor;
+
+	private Color originalColor;
+
+	// Use this for initialization
+	void Start () {
+		myRenderer = GetComponent<Renderer> ();
+		originalColor = myRenderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			myRenderer.material.color = touchColor;
+			isOverlaped = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			myRenderer.material.color = originalColor;
+			isOverlaped = false;
+		}
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			myRenderer.material.color = touchColor;
+			isOverlaped = true;
+		}
+	}
+}
