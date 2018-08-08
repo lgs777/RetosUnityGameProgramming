31a1a819b685a9e6a690ca73c487a9bb9e3c46a5
lgs777 <jeongc3@naver.com>
2018-08-09 00:06:53 +0900

    ChangeFile

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 10f;
-    public Rigidbody playerRigidbody;
 
+	public int speed = 10;
+	public Rigidbody PlayerRigidbody;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -14,35 +15,6 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        /*
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            playerRigidbody.AddForce(0, 0, speed);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            playerRigidbody.AddForce(-speed, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))    
-        {
-            playerRigidbody.AddForce(0, 0, -speed);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            playerRigidbody.AddForce(speed, 0, 0);
-        }*/
-
-        float inputX = Input.GetAxis("Horizontal");
-        float inputZ = Input.GetAxis("Vertical");
-        //  playerRigidbody.AddForce(speed * inputX, 0, speed * inputZ);
-        Vector3 velocity = new Vector3(inputX, 0, inputZ);
-        float fallSpeed = playerRigidbody.velocity.y;
-
-        // (inputX * speed, 0, inputZ * speed)
-        velocity = velocity * speed;
-        velocity.y = fallSpeed;
-        playerRigidbody.velocity = velocity;
-        
+		
 	}
 }
